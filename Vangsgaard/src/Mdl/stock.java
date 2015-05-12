74d629c2d8db8c3a8a803ae3fc0f2fe5c43a051e
AndersBrohus <andez@live.dk>
2015-05-12 12:21:47 +0200

    Stock created

--- Vangsgaard/src/Mdl/stock.java
+++ Vangsgaard/src/Mdl/stock.java
@@ -5,4 +5,63 @@ public class stock {
 	private product product;
 	private int amount;
 	private department department;
+	
+	public stock(int id, Mdl.product product, int amount,
+			Mdl.department department) {
+		super();
+		this.id = id;
+		this.product = product;
+		this.amount = amount;
+		this.department = department;
+	}
+	/**
+	 * @return the id
+	 */
+	public int getId() {
+		return id;
+	}
+	/**
+	 * @param id the id to set
+	 */
+	public void setId(int id) {
+		this.id = id;
+	}
+	/**
+	 * @return the product
+	 */
+	public product getProduct() {
+		return product;
+	}
+	/**
+	 * @param product the product to set
+	 */
+	public void setProduct(product product) {
+		this.product = product;
+	}
+	/**
+	 * @return the amount
+	 */
+	public int getAmount() {
+		return amount;
+	}
+	/**
+	 * @param amount the amount to set
+	 */
+	public void setAmount(int amount) {
+		this.amount = amount;
+	}
+	/**
+	 * @return the department
+	 */
+	public department getDepartment() {
+		return department;
+	}
+	/**
+	 * @param department the department to set
+	 */
+	public void setDepartment(department department) {
+		this.department = department;
+	}
+	
+	
 }
